75826bfa3f352dbd7582687f10ad42dfc6b39957
Sayali Hatwar <sayali.hatwar4321@gmail.com>
2026-03-20 14:12:08 +0530

    git push loop statement

---
+++ string_file/running_sum.py
@@ -0,0 +1,18 @@
+#l2 = list(range(1,100))
+
+#previousval = 0
+#for  l2value in l2:
+    #print(l2value)
+ #   currentval = previousval + l2value
+  #  previousval = currentval
+   # print(f"Previous value : {previousval}, Current value : {currentval}, l2value : {l2value}")
+    #print()
+
+
+
+t1 = ('a','b',{'aa':'er','we':'are'},[34,23])
+
+print(type(t1))
+l2 = list(t1)
+print(l2)
+print(type(l2))
